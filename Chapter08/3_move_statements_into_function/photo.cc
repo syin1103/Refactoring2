8a0766ab26f07e5671834f681e74091636d534e9
syin1103 <syin1103@gmail.com>
2025-04-25 10:32:37 +0800

    add 8.3: move statements into function
    Signed-off-by: syin1103 <syin1103@gmail.com>

---
+++ Chapter08/3_move_statements_into_function/photo.cc
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <string>
+
+struct Photo {
+  Photo(const std::string& ititle, const std::string& iloc,
+        const std::string& idate)
+      : title(ititle), location(iloc), date(idate) {}
+
+  std::string title;
+  std::string location;
+  std::string date;
+};
+
+struct Person {
+  Person(const std::string& iname, Photo iphoto) : name(iname), photo(iphoto) {}
+  std::string name;
+  Photo photo;
+};
+
+std::string RenderPhoto(Photo photo) { return ""; }
+
+std::string EmitPhotoData(Photo photo) {
+  std::string result = "";
+  result += ("<p> location: " + photo.location + "</p>");
+  result += ("<p> date: " + photo.date + "</p>");
+  return result + "\n";
+}
+
+std::string PhotoDiv(Photo photo) {
+  std::string result = "<p> title: " + photo.title + "</p>";
+  result += EmitPhotoData(photo);
+  return "<div>" + result + "</div>" + "\n";
+}
+
+std::string RenderPerson(Person person) {
+  std::string result = "";
+  result += ("<p>" + person.name + "</p>");
+  result += RenderPhoto(person.photo);
+  result += ("<p> title: " + person.photo.title + "</p>");
+  result += EmitPhotoData(person.photo);
+  return result + "\n";
+}
+
+int main() {
+  Photo photo("photo", "China", "20250101");
+  Person person("person", photo);
+
+  std::cout << RenderPerson(person) << std::endl;
+  std::cout << PhotoDiv(person.photo) << std::endl;
+
+  return 0;
+}
