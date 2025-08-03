5f89927090d1e39fd1a0ea1961c99654cbdcd8cc
syin1103 <syin1103@gmail.com>
2025-08-03 17:39:26 +0800

    add 6.2: inline function
    Signed-off-by: syin1103 <syin1103@gmail.com>

---
+++ Chapter06/2_inline_function/report_lines.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+#include <unordered_map>
+
+using LinesMap = std::unordered_map<std ::string, std::string>;
+
+struct Customer {
+  Customer(const std::string& cname, const std::string& cloc)
+      : name(cname), location(cloc) {}
+  ~Customer() {}
+
+  std::string name;
+  std::string location;
+};
+
+void GatherCustomerData(LinesMap& out, const Customer& customer) {
+  out.insert({"name", customer.name});
+  out.insert({"location", customer.location});
+}
+
+LinesMap ReportLines(const Customer& customer) {
+  LinesMap lines{};
+  GatherCustomerData(lines, customer);
+  return lines;
+}
+
+int main() {
+  Customer customer("Jack", "China");
+
+  LinesMap lines = ReportLines(customer);
+  for (const auto& pair : lines) {
+    std::cout << pair.first << ": " << pair.second << std::endl;
+  }
+
+  return 0;
+}
