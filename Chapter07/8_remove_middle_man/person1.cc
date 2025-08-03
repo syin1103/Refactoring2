fa464c1fa83f569d7dff78b61f62b9ffd554261e
syin1103 <syin1103@gmail.com>
2025-08-03 12:08:20 +0800

    add 7.8: remove middle man
    Signed-off-by: syin1103 <syin1103@gmail.com>

---
+++ Chapter07/8_remove_middle_man/person1.cc
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <string>
+
+class Manager {
+ public:
+  Manager(const std::string& name = "") : name_(name) {}
+  ~Manager() {}
+
+  std::string name() const { return name_; }
+
+ private:
+  std::string name_;
+};
+
+class Department {
+ public:
+  Department() {}
+  ~Department() {}
+
+  int charge_code() const { return charge_code_; }
+  void set_charge_code(int code) { charge_code_ = code; }
+
+  Manager manager() const { return manager_; }
+  void set_manager(Manager manager) { manager_ = manager; }
+
+ private:
+  int charge_code_ = 0;
+  Manager manager_;
+};
+
+class Person {
+ public:
+  Person(const std::string& name) : name_(name) {}
+  ~Person() {}
+
+  std::string name() const { return name_; }
+  Department department() const { return department_; }
+  void set_department(Department department) { department_ = department; }
+
+ private:
+  std::string name_;
+  Department department_;
+};
+
+int main() {
+  Department department;
+  department.set_manager({"Jack"});
+
+  Person person("Tom");
+  person.set_department(department);
+
+  Manager manager = person.department().manager();
+  std::cout << manager.name() << std::endl;
+
+  return 0;
+}
