67142ad9a3f4b0feb1acecd718aeb908a12f84ff
syin1103 <syin1103@gmail.com>
2025-08-04 11:23:00 +0800

    add 6.7: rename variable
    Signed-off-by: syin1103 <syin1103@gmail.com>

---
+++ Chapter06/7_rename_variable/company_name.cc
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <string>
+
+int main() {
+  std::string cpy_name = "Acme Gooseberries";
+  std::cout << cpy_name << std::endl;
+  return 0;
+}
---
+++ Chapter06/7_rename_variable/company_name1.cc
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <string>
+
+int main() {
+  std::string company_name = "Acme Gooseberries";
+  std::cout << company_name << std::endl;
+  return 0;
+}
